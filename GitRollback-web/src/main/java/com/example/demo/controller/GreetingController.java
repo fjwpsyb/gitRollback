5bd2fd107f4e27a25a173e573ed1796bc99fd631
fjwpsyb <350381204@qq.com>
2022-05-17 17:15:50 +0800

    回退版本测试

--- GitRollback-web/src/main/java/com/example/demo/controller/GreetingController.java
+++ GitRollback-web/src/main/java/com/example/demo/controller/GreetingController.java
@@ -27,4 +27,7 @@ public class GreetingController {
 		//这里返回的数据类型是String，但实际上更多的数据通过本函数中Model model传给了前端。返回值String也会被SpringMVC整合为一个ModelAndView，以供前端使用。（Controller可以返回多种数值，比如void、String、ModelAndView。同学们可以自行搜索学习）
 		return "greeting";
 	}
+
+
+
 }
